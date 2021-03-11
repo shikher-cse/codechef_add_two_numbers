6914ee6dbab3b18547817e51e7959cca94852c31
shikherraina <54113189+shikher-cse@users.noreply.github.com>
2021-03-11 17:30:36 +0530

    Add files via upload

---
+++ codechef_add_two_numbers.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+		ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int  n  , result ;
+	int a, b;
+	
+	cin>>n;
+	if(1<= n<=1000 )
+	{
+	
+		for(int i  =0;i<n;i++)
+    	{
+	
+      	cin>>a >>b ;
+      	if(0<= a <=10000 && 0<=b<=10000)
+      	{
+		
+        for(int i  = 0;i<n;i++)
+      {
+      	result = a+b;
+      	
+	  }
+}
+	  cout<< a+ b<<"\n";
+      
+      }  
+   }   
+
+	return 0;
+		
+}
